a380023bcc847b4c3aec5f68a488863f1918c7d1
Sergio <sergio.g.p@hotmail.es>
2024-10-11 16:21:06 +0200

    Toggle led

--- testScripts/buttonScripts/main.py
+++ testScripts/buttonScripts/main.py
@@ -7,17 +7,24 @@ LED_GPIO = 37
 SIGNAL_GPIO = 40
 
 
-def event_cb(event: ButtonEvent):
-    if event == ButtonEvent.PRESS:
-        print("Press event")
-        return
-    if event == ButtonEvent.RELEASE:
-        print("Release event")
-        return
+class button:
+    def __init__(self) -> None:
+        self.driver = ButtonDriver(LED_GPIO, SIGNAL_GPIO, self.event_cb)
+        self.ledState = 1
+        self.driver.led.set(self.ledState)
+
+    def event_cb(self, event: ButtonEvent):
+        if event == ButtonEvent.PRESS:
+            print("Press event")
+            return
+        if event == ButtonEvent.RELEASE:
+            print("Release event")
+            self.ledState ^= 1
+            self.driver.led.set(self.ledState)
+            return
 
 
-button = ButtonDriver(LED_GPIO, SIGNAL_GPIO, event_cb)
-button.led.on()
+button = button()
 
 while 1:
     time.sleep(0.1)
