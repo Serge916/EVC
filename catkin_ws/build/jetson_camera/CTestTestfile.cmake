7f073175cb9dad573e128d85be671e8b3ed94ef0
billakosrizeakos <v.rizeakos@student.tue.nl>
2024-11-01 15:46:55 +0100

    Removed duplicates xD

---
+++ catkin_ws/build/jetson_camera/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jetbot/EVC/catkin_ws/src/jetson_camera
+# Build directory: /home/jetbot/EVC/catkin_ws/build/jetson_camera
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
