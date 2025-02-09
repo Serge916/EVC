23f349967b23f87ccbb0a24d99a46515aac52a41
jetbot <v.rizeakos@student.tue.nl>
2025-02-09 06:29:25 -0600

    Changes in the camera node for less laggy implementation

--- workshops/vision_ws/src/jetson_camera/src/camera_publisher_node.py
+++ workshops/vision_ws/src/jetson_camera/src/camera_publisher_node.py
@@ -2,7 +2,6 @@
 
 import cv2
 import rospy
-import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import CompressedImage
 
@@ -17,8 +16,9 @@ class CameraPublisherNode:
 
         # Publisher for the image topic
         self.image_pub = rospy.Publisher(
-            "/camera/image_raw", CompressedImage, queue_size=1
-        )
+            '/camera/image_raw', 
+            CompressedImage,
+            queue_size=1)
 
         # Create a CvBridge object for converting images
         self.bridge = CvBridge()
@@ -26,16 +26,12 @@ class CameraPublisherNode:
         # Camera Dimensions And Properties Parameters
         config = self.load_yaml()
 
-        # For frame capturing (calibration scripts)
-        self.seqNo = 0
-        self.saveImages = True
-
         self.sensor_id = config["sensor_id"]  # 0: CSI, 1: USB
         self.width = config["width"]
         self.height = config["height"]
         self.fps = config["fps"]
         self.flip_method = config["flip_method"]
-
+        
         # GStreamer pipeline for the Jetson CSI camera
         self.pipeline = self.gstreamer_pipeline()
         if self.pipeline is None:
@@ -54,39 +50,31 @@ class CameraPublisherNode:
     def gstreamer_pipeline(self):
         if self.sensor_id == 0:
             # Define the GStreamer pipeline for the Jetson camera
-            return (
-                "nvarguscamerasrc sensor-id=0 ! "
-                "video/x-raw(memory:NVMM), "
-                "width=(int)%d, height=(int)%d, "
-                "format=(string)NV12, framerate=(fraction)%d/1 ! "
-                "nvvidconv flip-method=%d ! "
-                "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-                "videoconvert ! "
-                "video/x-raw, format=(string)BGR ! appsink"
-                % (
-                    self.width,
-                    self.height,
-                    self.fps,
-                    self.flip_method,
-                    self.width,
-                    self.height,
-                )
-            )
+            return ('nvarguscamerasrc sensor-id=0 ! '
+                    'video/x-raw(memory:NVMM), '
+                    'width=(int)%d, height=(int)%d, '
+                    'format=(string)NV12, framerate=(fraction)%d/1 ! '
+                    'nvvidconv flip-method=%d ! '
+                    'video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! '
+                    'videoconvert ! '
+                    'video/x-raw, format=(string)BGR ! appsink' % (self.width, self.height, self.fps, 
+                                                                    self.flip_method, self.width, self.height))
         elif self.sensor_id == 1:
-            return (
-                "v4l2src device=/dev/video1 ! "
-                "video/x-raw, "
-                "width=(int)%d, height=(int)%d, "
-                "format=(string)YUY2, framerate=(fraction)%d/1 ! "
-                "videoconvert ! "
-                "video/x-raw, format=BGR ! "
-                "appsink" % (self.width, self.height, self.fps)
-            )
+            return ('v4l2src device=/dev/video1 ! '
+                    'video/x-raw, '
+                    'width=(int)%d, height=(int)%d, '
+                    'format=(string)YUY2, framerate=(fraction)%d/1 ! '
+                    'videoconvert ! '
+                    'video/x-raw, format=BGR ! '
+                    'appsink' % (self.width, self.height, self.fps))
         else:
             return None
 
     def start_publishing(self):
         rate = rospy.Rate(self.fps)
+        msg = CompressedImage()
+        msg.format = "jpeg"
+
         while not rospy.is_shutdown():
             ret, frame = self.cap.read()
             if not ret:
@@ -94,28 +82,15 @@ class CameraPublisherNode:
                 continue
             if self.first_image_received == False:
                 self.first_image_received = True
-                rospy.loginfo(
-                    "Camera publisher captured first image from physical device."
-                )
+                rospy.loginfo("Camera publisher captured first image from physical device.")
             try:
                 # Convert the OpenCV image to a ROS CompressedImage message
-                msg = CompressedImage()
-                msg.header.stamp = rospy.Time.now()
-                msg.format = "jpeg"
-                msg.data = np.array(cv2.imencode(".jpg", frame)[1]).tostring()
-
-                # Publish the image
-                self.image_pub.publish(msg)
-
-                if self.saveImages:
-                    # rospy.loginfo("Printing frame to {}".format(os.getcwd()))
-                    cv2.imwrite(
-                        "/home/jetbot/workshops/workshopTwo_Assignment/calibration/frames/frame_{seqNo}.jpg".format(
-                            seqNo=self.seqNo
-                        ),
-                        frame,
-                    )
-                    self.seqNo += 1
+                success, encoded_image = cv2.imencode(".jpg", frame)
+                if success:
+                    msg.header.stamp = rospy.Time.now()
+                    msg.data = encoded_image.tobytes()
+                    # Publish the image
+                    self.image_pub.publish(msg)
 
             except CvBridgeError as e:
                 rospy.logerr("Error converting image: {}".format(e))
@@ -138,7 +113,6 @@ class CameraPublisherNode:
         rospy.loginfo("Loaded config: %s", config)
 
         return config
-
 
 if __name__ == "__main__":
     node_name = "camera_publisher_node"
